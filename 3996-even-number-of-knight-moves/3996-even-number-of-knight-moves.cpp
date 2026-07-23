ac0236d9fc417a20d0e79637806cbdd0a4924cb7
Mohit Kumar <133407895+Mohit2004Gothwal@users.noreply.github.com>
2026-07-23 22:55:06 +0530

    Time: 0 ms (100%), Space: 49.8 MB (39.37%) - LeetHub

---
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool canReach(vector<int>& start, vector<int>& target) {
+        int st = start[0]+start[1];
+        int end = target[0]+target[1];
+
+        return (st%2 == 0 && end%2 == 0) || (st%2 != 0 && end%2 != 0);
+    }
+};
