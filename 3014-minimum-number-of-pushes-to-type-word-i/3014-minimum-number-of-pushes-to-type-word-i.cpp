95437cb395b022daf6322787d1e386ad47e86032
Mohit Kumar <133407895+Mohit2004Gothwal@users.noreply.github.com>
2026-07-30 17:22:16 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.83%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    // First 8 letters push 1 time 
+    // Next 8 letters push 2 time
+    //  Next 8 letters push 3
+    // Next 8 letters push 4 time
+
+    // cost is simply (i/8)+1;
+    int minimumPushes(string word) {
+        int cost = 0;
+        for(int i=0;i<word.size();i++){
+            cost += (i/8)+1;
+        }
+        return cost;
+        
+    }
+};
